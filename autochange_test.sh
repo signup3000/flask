f7c4c0a88b90c389b6ba2d239ce2ba561ae5a41d
Gary <gear@thytruth.com>
2017-03-29 17:34:28 -0700

    testing change new string P0Fp9iPL

--- autochange_test.sh
+++ autochange_test.sh
@@ -1,1 +1,8 @@
-echo "test auto_change" >> README.md ; git commit -a -m "testing change"; git push
+
+export NEWSTRING=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
+echo $NEWSTRING >> README.md
+cd templates
+cp -rp .index.html index.html
+sed -i -- 's|'__search_and_replace_here'|'${NEWSTRING}'|g' index.html
+cd -
+git commit -a -m "testing change new string $NEWSTRING"; git push
